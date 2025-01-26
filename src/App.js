89941a03614cd98ad67b071e020c844f87e8473f
VishalChoudhary <vishalghn953@gmail.com>
2025-01-26 12:49:52 +0530

    makeing NoteList/Note dynamic & destructuring commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState } from "react";
+import { nanoid } from 'nanoid';
 import NotesList from "./components/NotesLists";
 
 const App = () =>{
+  const [notes,setNotes] = useState([
+    {
+    id: nanoid(),
+    text: "First Note",
+    date: "15/01/2024"
+    },
+    {
+      id: nanoid(),
+      text: "Second Note",
+      date: "15/01/2024"
+    },
+    {
+      id: nanoid(),
+      text: "Third Note",
+      date: "15/01/2024"
+    },
+    {
+      id: nanoid(),
+      text: "Fourth Note",
+      date: "15/01/2024"
+    }
+  ]);
   return (
     <div className="container">
-      <NotesList/>
+      <NotesList notes={notes}/>
     </div>
   );
 };
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,11 +1,11 @@
 import {MdDeleteForever} from 'react-icons/md';
 
-const Note = () =>{
+const Note = ({id,text,date}) =>{
     return(
         <div className="note">
-            <span>Text Area</span>
+            <span>{text}</span>
             <div className="note-footer">
-                <small>15/01/2025</small>
+                <small>{date}</small>
                 <MdDeleteForever className='delete-icon' size='1.3em'/>
             </div>
         </div>
